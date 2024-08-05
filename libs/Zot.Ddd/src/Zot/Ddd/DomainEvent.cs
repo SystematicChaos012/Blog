92a1b33ecb9878b895ef1e94c14b42704eaa4ab9
SystematicChaos <clooooooooop@gmail.com>
2024-08-06 00:51:55 +0800

    Init Zot.Ddd solution

---
+++ libs/Zot.Ddd/src/Zot/Ddd/DomainEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace Zot.Ddd;
+
+/// <summary>
+/// Domain event
+/// </summary>
+public abstract class DomainEvent
+{
+    /// <summary>
+    /// The timestamp when the domain event was created
+    /// </summary>
+    public DateTime TimeStamp { get; } = DateTime.UtcNow;
+}
---
+++ libs/Zot.Ddd/src/Zot/Ddd/GeneratedAggregateRootAttribute.cs
@@ -0,0 +1,5 @@
+﻿namespace Zot.Ddd;
+
+/// <summary>Instructs the Zot.Ddd source generator to generate methods and properties for an aggregate root.</summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+public sealed class GeneratedAggregateRootAttribute : Attribute;
